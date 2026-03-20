708af174fe36042e6faa72ea174af5ceeb428022
moul <94029+moul@users.noreply.github.com>
2026-03-20 13:58:24 +0000

    fix: add no-cache header on index to prevent stale JS

--- web/server.go
+++ web/server.go
@@ -270,6 +270,7 @@ func (s *Server) handleIndex(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 	w.Write(data)
 }
 
